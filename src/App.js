adb1fea5959a94cfa837f0402344e4f894c4cbc0
FidelStucht <128236556+FidelStucht@users.noreply.github.com>
2023-05-13 15:24:27 +0200

    create button

--- src/App.js
+++ src/App.js
@@ -5,9 +5,11 @@ export default function App() {
   return (
     <div className="App">
       <h1>Weather App</h1>
+      <button className="btn btn-primary">Hello</button>
       <footer>
         This project was coded by Fidelis Stuchtey and is
         <a href="https://github.com/FidelStucht/my-react-app" target="-blank">
+          {" "}
           open sourced
         </a>
       </footer>
