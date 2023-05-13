decbe997f1addc150f0d046124401824bc833fa0
FidelStucht <128236556+FidelStucht@users.noreply.github.com>
2023-05-13 15:58:28 +0200

    HTML actual weather

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
+import Weather from "./Weather";
 
 export default function App() {
   return (
     <div className="App">
       <h1>Weather App</h1>
-      <button className="btn btn-primary">Hello</button>
+      <Weather />
+
       <footer>
         This project was coded by Fidelis Stuchtey and is
         <a href="https://github.com/FidelStucht/my-react-app" target="-blank">
