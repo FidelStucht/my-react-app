57875be8ead3fc52581fad9be23afc5cc02758cf
FidelStucht <128236556+FidelStucht@users.noreply.github.com>
2023-05-13 15:07:52 +0200

    added Github url

--- src/App.js
+++ src/App.js
@@ -2,5 +2,15 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by Fidelis Stuchtey and is
+        <a href="https://github.com/FidelStucht/my-react-app" target="-blank">
+          open sourced
+        </a>
+      </footer>
+    </div>
+  );
 }
